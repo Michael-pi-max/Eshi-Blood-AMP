7c5c64eb4254b71d562fc6458ad4fec419545917
Abenezer Atnafu <se.abenezer.atnafu@gmail.com>
2021-08-21 21:21:03 +0300

    Users role added to the schema, privileges to users(permission) added, it also ref to User model

---
+++ server/src/models/role.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+/**
+ * Schema for storing role
+ *  * roleName - role name
+ *  * actions - role description
+ *  *
+ */
+const schema = new mongoose.Schema(
+  {
+    roleName: {
+      type: String,
+    },
+
+    privileges: {
+      type: [
+        {
+          title: { type: String },
+          permissions: {
+            type: [],
+            default: ["read", "create", "update", "delete"],
+          },
+        },
+      ],
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const Role = mongoose.model("Role", schema);
+
+module.exports = Role;
