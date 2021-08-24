8e2d66e9237a2806134055795a55c8aa2b3dd852
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-08-24 10:46:34 +0300

    user router is created

---
+++ server/src/routes/user.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const userController = require("../controllers/user");
+const userValidation = require("../middleware/validation/user");
+const { verifyUser, verifyRole } = require("../middleware/auth");
+const uploadImage = require("../controllers/uploadImage");
+
+const router = express.Router();
+
+router.post("/login", userValidation.validate("LOGIN"), userController.login);
+
+router.post(
+  "/signup",
+  uploadImage,
+  userValidation.validate("SIGNUP"),
+  userController.signup
+);
+
+router
+  .get(
+    "/:id",
+    verifyUser,
+    verifyRole("user", "read", "user"),
+    userValidation.validate("GET"),
+    userController.getUserById
+  )
+  .patch(
+    "/:id",
+    verifyUser,
+    verifyRole("user", "update", "user"),
+    userValidation.validate("UPDATE"),
+    userController.updateUser
+  );
+
+router.get(
+  "/",
+  verifyUser,
+  verifyRole("user", "read", "user"),
+  userValidation.validate("GET"),
+  userController.getAllUsers
+);
+
+router.get("/search", verifyUser, userController.searchUserByName);
+
+module.exports = router;
