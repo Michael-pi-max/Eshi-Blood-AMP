f92bad845df07d07589c63024bb7519526a38301
Semere Talegngeta <se.semere.talegngeta@gmail.com>
2021-08-25 23:26:09 +0300

    appointment route created

---
+++ server/src/routes/appointment.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { verifyUser, verifyRole } = require("../middleware/auth");
+const appointmentController = require("../controllers/appointment");
+const appointmentValidation = require("../middleware/validation/appointment");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(verifyUser, appointmentController.getAllAppointments)
+  .post(
+    verifyUser,
+    appointmentValidation.validate("CREATE"),
+    appointmentController.createAppointment
+  );
+router
+  .route("/:id")
+  .get(
+    verifyUser,
+    appointmentValidation.validate("GET"),
+    appointmentController.getAppointment
+  )
+  .patch(
+    verifyUser,
+    verifyRole("user", "update", "appointment"),
+    appointmentValidation.validate("UPDATE"),
+    appointmentController.updateAppointment
+  )
+  .delete(
+    verifyUser,
+    verifyRole("user", "delete", "appointment"),
+    appointmentValidation.validate("DELETE"),
+    appointmentController.deleteAppointment
+  );
+//appointment by user
+router
+  .route("/user/:userId")
+  .get(verifyUser, appointmentController.getUserAppointment);
+
+//appointment by donation center
+router.route(
+  "/donationCenter/:donationCenterId",
+  verifyUser,
+  appointmentController.getDonationCenterAllAppointments
+);
+module.exports = router;
