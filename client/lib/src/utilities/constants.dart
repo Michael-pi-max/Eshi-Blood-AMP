6df564bb6cb54f25af9e7335c81065739dbc22ab
Miheretu Teshale <se.abenezer.atnafu@gmail.com>
2021-08-27 12:53:32 +0300

    initial client setup

---
+++ client/lib/src/utilities/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static final String url = "http://127.0.0.1:8000";
+  static final String baseUrl = "http://127.0.0.1:8000/api/v1";
+  static final String emuBaseUrl = "http://10.0.2.2:8000/api/v1";
+  static final String imageBaseUrl = "http://10.0.2.2:8000/";
+}
+
